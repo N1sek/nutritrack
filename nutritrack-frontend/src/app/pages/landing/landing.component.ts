c195e4a89a1201a85f40c8a92d4d7d6151f41579
Denis Cizma <denisrobcizma@gmail.com>
2025-02-14 11:58:29 +0100

    .

---
+++ nutritrack-frontend/src/app/pages/landing/landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss',
+  standalone: true
+})
+export class LandingComponent {
+
+}
