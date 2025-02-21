9f97af344b950108944fb07751da0fdd0b6af463
N1sek <denisrobcizma@gmail.com>
2025-02-21 02:00:29 +0100

    Paso, me voy a dormir

--- nutritrack-frontend/src/app/pages/dashboard/dashboard.component.ts
+++ nutritrack-frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from '../../shared/components/navbar/navbar.component';
+import {BoxComponent} from '../../shared/components/box/box.component';
+import {PesoComponent} from '../../shared/components/peso/peso.component';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    BoxComponent,
+    PesoComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
